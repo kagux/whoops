8a57e35b7f93e0eb8c25f25041055237367d9f67
Boris Mikhaylov <kaguxmail@gmail.com>
2012-05-19 12:40:44 -0700

    added engine name and changed route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Whoops::Engine.routes.draw do
-  resources :event_groups, :as => "whoops_event_groups" do 
+  resources :event_groups, :as => "event_groups" do
     resources :events
   end
   
-  resources :events, :as => "whoops_events"
+  resources :events, :as => "events"
   root :to => "event_groups#index"
 end
--- lib/whoops/engine.rb
+++ lib/whoops/engine.rb
@@ -1,5 +1,6 @@
 module Whoops
   class Engine < Rails::Engine
+    engine_name 'whoops'
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, File.expand_path("../../../app/assets/javascripts", __FILE__)
     end
